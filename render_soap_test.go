9652436e991d9139c3efde1c5b0ba0963265b977
Jeff Deville <jeffdeville@gmail.com>
2014-12-05 16:47:57 -0500

    skeleton for render_soap

---
+++ render_soap_test.go
@@ -0,0 +1,12 @@
+package parse_xml_test
+
+import (
+	. "github.com/jeffdeville/parse_xml"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("RenderSoap", func() {
+	// How should this be tested?
+})
